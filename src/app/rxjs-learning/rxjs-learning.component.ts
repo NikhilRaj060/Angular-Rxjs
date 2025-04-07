359fb7e9ea7d0118a6fd1b3b4be44694195b1306
Nikhil Raj <nikhilraj2061@gmail.com>
2025-04-07 08:03:47 +0530

    `Added Angular CLI documentation to README.md`

---
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-learning',
+  imports: [],
+  templateUrl: './rxjs-learning.component.html',
+  styleUrl: './rxjs-learning.component.scss',
+  standalone: true
+})
+export class RxjsLearningComponent {
+
+}
